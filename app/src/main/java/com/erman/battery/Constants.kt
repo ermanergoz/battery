69006c855f81fd392db870ad8ae5f174595585e3
Yusuf Erman ERGÖZ <erman.ergoz@hotmail.com>
2020-06-28 04:28:29 +0200

    Revert "Add BroadcastListener to update the ui"
    This reverts commit 5248a32453155bee9913004da68327e01749d409.

--- app/src/main/java/com/erman/battery/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.erman.battery
-
-const val KEY_BUNDLE_BATTERY_PERCENTAGE: String = "key_bundle_battery_percentage"
-const val KEY_BUNDLE_BATTERY_VOLTAGE: String = "key_bundle_battery_voltage"
-const val KEY_BUNDLE_BATTERY_TEMP: String = "key_bundle_battery_temp"
